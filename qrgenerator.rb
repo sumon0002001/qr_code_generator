6489ffa9545bffda0790d0e26d22ae3d8b4a2745
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-04-16 12:31:20 +0600

    Add dynamic values for url and color

--- qrgenerator.rb
+++ qrgenerator.rb
@@ -1,9 +1,12 @@
 require 'rqrcode'
 
-qr_code = RQRCode::QRCode.new("http://mirrawshanali.xyz/")
+url = ARGV[0]
+color = ARGV[1] ? "##{ARGV[1]}" : 'black'
+
+qr_code = RQRCode::QRCode.new(url)
 
 png = qr_code.as_png(
-    color: "black",
+    color: color,
     fill: "white",
     size: 300
 )
